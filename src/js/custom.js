e95863a623b5e0ae1f9b585df368ea1593f9fa59
Talha <talha@extrasol.co.uk>
2024-05-03 20:10:39 +0500

    talha

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,21 +6,39 @@ $(document).ready(function () {
   //   if (preloader) {
   //     preloader.style.display = "none";
   //   }
-});
 
-// Get all checkboxes
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  // Get all checkboxes
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-// Function to update selected count
-function updateSelectedCount() {
-  const selectedCheckboxes = document.querySelectorAll(
-    'input[type="checkbox"]:checked'
-  );
-  document.getElementById("selectedCount").textContent =
-    selectedCheckboxes.length;
-}
+  // Function to update selected count
+  function updateSelectedCount() {
+    const selectedCheckboxes = document.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+    document.getElementById("selectedCount").textContent =
+      selectedCheckboxes.length;
+  }
 
-// Attach change event listener to each checkbox
-checkboxes.forEach(function (checkbox) {
-  checkbox.addEventListener("change", updateSelectedCount);
+  // Attach change event listener to each checkbox
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", updateSelectedCount);
+  });
+
+  const fileInput = document.getElementById("file-input");
+  console.log(fileInput);
+  const imagePreview = document.getElementById("img-preview");
+  const toast = document.getElementById("toast");
+
+  fileInput.addEventListener("change", (e) => {
+    if (e.target.files.length) {
+      const src = URL.createObjectURL(e.target.files[0]);
+      imagePreview.src = src;
+      showToast();
+    }
+  });
+
+  function showToast() {
+    toast.classList.add("show");
+    setTimeout(() => toast.classList.remove("show"), 3000);
+  }
 });
