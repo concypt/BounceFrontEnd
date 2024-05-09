85fa49d1b1044fb681db7b2dc7fc7dc1bf116adb
concypt <imran@concypt.com>
2024-05-09 03:39:33 +0100

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -26,27 +26,30 @@ import Marketing from "./pages/Dashboard/Marketing.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/events/:eventId" element={<EventDetail />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/verification" element={<Verification />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/host-profile/:organisationId" element={<HostProfile />} />
+    <>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:eventId" element={<EventDetail />} />
+        <Route path="/host-profile/:organisationId" element={<HostProfile />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+      {/* Should be in dashboard layout  */}
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route
-        path="/forgot-password-request"
-        element={<ForgotPasswordRequest />}
-      />
-
-      <Route path="/forgot-password-reset" element={<ForgotPasswordReset />} />
       <Route path="/dashboard-host" element={<Host />} />
       <Route path="/dashboard-event" element={<Event />} />
       <Route path="/dashboard-single-event" element={<EventSingle />} />
       <Route path="/dashboard-marketing" element={<Marketing />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Route>
+      {/* without layout pages */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/verification" element={<Verification />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/forgot-password-request"
+        element={<ForgotPasswordRequest />}
+      />
+      <Route path="/forgot-password-reset" element={<ForgotPasswordReset />} />
+    </>
   )
 );
 function App() {
