397cd17e77404af93fb480e19ad1b62fda319035
usman-extrasole <usman.mughal@extrasol.co.uk>
2024-05-17 19:55:45 +0500

    done

--- src/pages/static/Contact.jsx
+++ src/pages/static/Contact.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 import styles from "./contact.module.css";
 import { Link, NavLink } from "react-router-dom";
 import "react-date-range/dist/styles.css"; // Main css file
@@ -11,32 +13,84 @@ import calender from "../../assets/images/calender.svg";
 import promoteImg from "../../assets/images/promote_img.png";
 
 function News() {
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false); 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    fetch("https://bounce.extrasol.co.uk/api/attenders/contact-us", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Api-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9",
+        
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setIsSubmitting(false); 
+        
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: "Message sent successfully!",
+          timer: 2000,
+        }).then(() => {
+          setFormData({ // Reset form fields to empty values
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          });
+          navigate("/contact");
+        });
+      })
+      .catch((error) => {
+        console.error("Error submitting host application:", error);
+        setIsSubmitting(false); 
+        // Handle error
+      });
+  };
   return (
     <>
       <div className="bounce_bg_circle">
         <div className={styles.aboutMain}>
           <dir className={`header_div ${styles.headerBlog}`}>
-            <div class={styles.cardContainer}>
+            <div className={styles.cardContainer}>
               <h1>Contact Us</h1>
               <div className={styles.contactForm}>
-                <form action="submit" className={styles.formInput}>
+                <form onSubmit={handleSubmit} className={styles.formInput}>
                   <div className={styles.contactInput}>
                     <div className={styles.contactFields}>
                       <label htmlFor="">Your Name</label>
-                      <input type="text" placeholder="Name" required />
+                      <input type="text" placeholder="Name" value={formData.name} name="name" onChange={handleChange} required />
                     </div>
                     <div className={styles.contactFields}>
                       <label htmlFor="">Your Email</label>
-                      <input type="email" placeholder="Email" required />
+                      <input type="email" placeholder="Email" value={formData.email}  name="email" onChange={handleChange} required />
                     </div>
                   </div>
                   <div className={styles.contactFields}>
                     <label htmlFor="">Subject</label>
-                    <input type="text" />
+                    <input type="text" placeholder="Subject" name="subject" value={formData.subject} onChange={handleChange}/>
                   </div>
                   <div className={styles.contactFields}>
                     <label htmlFor="">Message</label>
-                    <textarea name="" id=""></textarea>
+                    <textarea placeholder="Message" name="message" value={formData.message} onChange={handleChange} id=""></textarea>
                   </div>
                   <div className={styles.contactInfo}>
                     <Link>
@@ -50,9 +104,11 @@ function News() {
                       <button
                         type="submit"
                         className={`global_button_one ${styles.submitBtn}`}
+                        disabled={isSubmitting} 
                       >
                         {" "}
-                        <span>Send Message</span>
+                      
+                        <span>{isSubmitting ? 'Sending...' : 'Send Message'}</span>
                       </button>
                     </Reveal>
                   </div>
