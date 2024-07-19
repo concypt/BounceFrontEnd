0753062b19663ff464e5bb7f6873ed9bc74d4d51
Masab Ahmad <musabrj@gmail.com>
2024-07-19 20:57:28 +0500

    checkout

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,15 @@
+import CheckoutPayment from "../components/CheckoutPayment";
+
+const Checkout = () => {
+  return (
+    <>
+      <div className="bounce_bg_circle">
+        <div className="header_div checkout">
+          <CheckoutPayment />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
